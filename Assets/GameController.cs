861bfb1893bb2d3b2dc97ac65fc782aabd2ef194
Darenn Keller <keller.darenn@gmail.com>
2016-12-11 11:50:36 +0100

    Motodachi modelized. Idle, walk and men animation done. For now, the motodachi loop on men, and we can block it with Up arrow.

---
+++ Assets/GameController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public struct Hit {
+	public string Name;
+	public string AnimationName;
+	public string [] ButtonsName;
+}
+
+public class GameController : MonoBehaviour {
+
+	public Hit [] Hits;
+
+	Animator anim;
+
+	void Awake () {
+		anim = gameObject.GetComponent<Animator> ();
+	}
+
+	void Start () {
+		StartCoroutine (RandomHits ());
+	}
+
+	bool timeToBlock = false;
+	Hit currentHit;
+
+	void Update () {
+		if (timeToBlock) {
+			if (Input.anyKeyDown) {
+				bool allButtonsPressed = true;
+				foreach (string buttonName in currentHit.ButtonsName) {
+					if (!Input.GetButtonDown(buttonName)) {
+						allButtonsPressed = false;
+						break;
+					}
+				}
+				if (allButtonsPressed) {
+					hitBlocked ();		
+				}
+			}
+		}
+	}
+
+	bool hitBlockedInTime = false;
+
+	void makeHit (Hit hit) {
+		hitBlockedInTime = false;
+		timeToBlock = false;
+		anim.SetTrigger (hit.AnimationName);
+		currentHit = hit;
+	}
+
+	void hitBlocked () {
+		timeToBlock = false;
+		hitBlockedInTime = true;
+		Debug.Log ("Hit blocked !");
+	}
+
+	/// <summary>
+	/// The animation call this function when it's time to push the buttons to block.
+	/// So it's the moment I put colors on UI.
+	/// </summary>
+	public void BlockStarting () {
+		timeToBlock = true;
+	}
+
+	/// <summary>
+	/// The animation call this function when it's to late to push the buttons to block.
+	/// </summary>
+	public void BlockEnding () {
+		timeToBlock = false;
+		if (!hitBlockedInTime) {
+			Debug.Log ("Hit taken !");
+		}
+	}
+
+	IEnumerator RandomHits () {
+		while (true) {
+			makeHit (Hits [Random.Range(0, Hits.Length)]);
+			yield return new WaitForSeconds (4f);
+		}
+	}
+}
---
+++ Assets/MotodachiMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotodachiMovement : MonoBehaviour {
+
+	public float WalkSpeed;
+	public float WalkDistance;
+	public float MenSpeed;
+
+	Animator anim;
+
+	void Awake () {
+		anim = gameObject.GetComponent<Animator> ();
+	}
+
+	public void MoveForward () {
+		anim.SetTrigger ("MoveForward");
+		Vector3 targetPosition = transform.position + (transform.forward * WalkDistance);
+		StartCoroutine (move (targetPosition));
+	}
+
+	public void Men () {
+		anim.SetTrigger ("Men");
+		Vector3 targetPosition = transform.position + (transform.forward * WalkDistance);
+		StartCoroutine (move (targetPosition));
+	}
+
+	IEnumerator move (Vector3 targetPosition) {
+		while (transform.position != targetPosition) {
+			transform.position = Vector3.Lerp (transform.position, targetPosition, WalkSpeed * Time.deltaTime);
+			yield return new WaitForFixedUpdate ();
+		}
+	}
+
+
+}

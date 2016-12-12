e26687e50d313b5b3e745e08c797c9d65d7f5a03
Darenn Keller <keller.darenn@gmail.com>
2016-12-12 21:51:58 +0100

    Final commit.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 using System.Collections;
 
 [System.Serializable]
@@ -6,37 +8,96 @@ public struct Hit {
 	public string Name;
 	public string AnimationName;
 	public string [] ButtonsName;
+	public int Value;
 }
 
 public class GameController : MonoBehaviour {
 
 	public Hit [] Hits;
+	public UIController uic;
+	public int Score;
+	public int Health = 3;
+	public int numHitsToUpgradeDifficulty = 4;
+	public float Difficulty = 1.0f;
+	public float TimeBetweenAttacks = 4f;
+	bool gameOver = false;
+	int hitCount = 0;
+
+	public AudioClip[] clips;
+	public AudioClip[] woodShocks;
+	public AudioClip hurt;
+	public AudioSource audioSource;
+	public AudioSource audioSourceHit;
 
 	Animator anim;
+	List<string> possibleButtons;
 
 	void Awake () {
+		possibleButtons = new List<string> (new string[] {"Arms.Up", "Arms.Down", "Arms.Right", "Arms.Left", "Legs.Up", "Legs.Down", "Legs.Left", "Legs.Right"});
 		anim = gameObject.GetComponent<Animator> ();
 	}
 
 	void Start () {
-		StartCoroutine (RandomHits ());
+		uic.UpdateHealth (Health);
 	}
 
 	bool timeToBlock = false;
 	Hit currentHit;
+	List<string> buttonsToPush = new List<string> ();
+	List<string> buttonsPushed = new List<string> ();
+	bool wrongButtonPressed = false;
+	bool hitStarted = false;
+	bool pressTooEarly = false;
 
 	void Update () {
+		if (Input.GetButtonDown ("Cancel")) {
+			Application.Quit ();
+		}
 		if (timeToBlock) {
 			if (Input.anyKeyDown) {
+				wrongButtonPressed = true;
+				foreach (string buttonName in possibleButtons) {
+					if (buttonsToPush.Contains (buttonName)) {
+						if (Input.GetButtonDown (buttonName) && !buttonsPushed.Contains (buttonName)) {
+							buttonsPushed.Add (buttonName);
+							uic.HighlightPressedColor (buttonName);
+							wrongButtonPressed = false;
+						}
+					} else {
+						// If the button is pushed but is not in buttons to push
+						if (Input.GetButtonDown (buttonName)) {
+							wrongButtonPressed = true;
+							uic.HighlightErrorColor (buttonName);
+
+							break;
+						}
+					}
+				}
+				if (wrongButtonPressed) {
+					timeToBlock = false;
+					hitTaken ();
+					return;
+				}
 				bool allButtonsPressed = true;
-				foreach (string buttonName in currentHit.ButtonsName) {
-					if (!Input.GetButtonDown(buttonName)) {
+				foreach (string buttonName in buttonsToPush) {
+					// The button should be down, and pressed during the time to block
+					if (!Input.GetButton (buttonName) || !buttonsPushed.Contains (buttonName)) {
+						uic.HighlightHint (buttonName);
 						allButtonsPressed = false;
-						break;
 					}
 				}
 				if (allButtonsPressed) {
-					hitBlocked ();		
+					hitBlocked ();
+				}
+			}
+		} else if (hitStarted && !pressTooEarly) {
+			foreach (string buttonName in possibleButtons) {
+				if (Input.GetButtonDown (buttonName)) {
+					uic.HighlightErrorColor (buttonName);
+					uic.ShowTooEarly ();
+					pressTooEarly = true;
+					hitTaken ();
+					break;
 				}
 			}
 		}
@@ -45,16 +106,50 @@ public class GameController : MonoBehaviour {
 	bool hitBlockedInTime = false;
 
 	void makeHit (Hit hit) {
+		uic.ResetToNomalSituation ();
+		pressTooEarly = false;
 		hitBlockedInTime = false;
 		timeToBlock = false;
+		wrongButtonPressed = false;
+		buttonsToPush = new List<string> (hit.ButtonsName);
+		buttonsPushed.Clear ();
 		anim.SetTrigger (hit.AnimationName);
+		hitStarted = true;
 		currentHit = hit;
+		foreach (var buttonName in hit.ButtonsName) {
+			uic.HighlightHint (buttonName);
+		}
+		hitCount++;
+		if (hitCount >= numHitsToUpgradeDifficulty) 
+		{
+			hitCount = 0;
+			Difficulty += 0.1f;
+			anim.SetFloat ("Multiplicateur", Difficulty);
+			if (TimeBetweenAttacks >= 2.2f) {
+				TimeBetweenAttacks -= 0.2f;
+			}
+		}
 	}
 
 	void hitBlocked () {
 		timeToBlock = false;
 		hitBlockedInTime = true;
-		Debug.Log ("Hit blocked !");
+		uic.ShowParade();
+		Score += currentHit.Value;
+		uic.UpdateScore (Score);
+		hitStarted = false;
+		playShock ();
+	}
+
+	void hitTaken() {
+		uic.ShowMissed ();
+		Health--;
+		uic.UpdateHealth (Health);
+		hitStarted = false;
+		playHurt ();
+		if (Health <= 0) {
+			StartCoroutine (DoGameOver ());
+		}
 	}
 
 	/// <summary>
@@ -62,6 +157,13 @@ public class GameController : MonoBehaviour {
 	/// So it's the moment I put colors on UI.
 	/// </summary>
 	public void BlockStarting () {
+		playCry ();
+		if (pressTooEarly) {
+			return;
+		}
+		foreach (var buttonName in buttonsToPush) {
+			uic.HighlightToPressColor (buttonName);
+		}
 		timeToBlock = true;
 	}
 
@@ -70,15 +172,64 @@ public class GameController : MonoBehaviour {
 	/// </summary>
 	public void BlockEnding () {
 		timeToBlock = false;
-		if (!hitBlockedInTime) {
-			Debug.Log ("Hit taken !");
+		// If the wrong button is pressed hitTaken was already called
+		if (!hitBlockedInTime && !wrongButtonPressed && !pressTooEarly) {
+			uic.ShowTooLate ();
+			hitTaken ();
 		}
 	}
 
-	IEnumerator RandomHits () {
-		while (true) {
+	public IEnumerator RandomHits () {
+		yield return new WaitForSeconds (2f);
+		while (!gameOver) {
 			makeHit (Hits [Random.Range(0, Hits.Length)]);
-			yield return new WaitForSeconds (4f);
+			yield return new WaitUntil ( () => hitStarted);
+			yield return new WaitForSeconds (TimeBetweenAttacks);
 		}
+	}
+
+	IEnumerator ReloadScene () {
+		yield return new WaitForSeconds (6f);
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+
+	public void StartGame () {
+		Score = 0;
+		Health = 3;
+		uic.UpdateScore (Score);
+		uic.UpdateHealth (Health);
+		gameOver = false;
+		TimeBetweenAttacks = 4f;
+		hitCount = 0;
+		Difficulty = 1f;
+		StartCoroutine (RandomHits ());
+	}
+
+	IEnumerator DoGameOver () {
+		gameOver = true;
+		uic.ShowGameOver ();
+		yield return new WaitForSeconds (4f);
+		uic.ResetToNomalSituation ();
+		uic.DisplayMenu ();
+	}
+
+	void playCry () {
+		AudioClip cry = clips [Random.Range (0, clips.Length)];
+		audioSource.clip = cry;
+		audioSource.pitch = Random.Range (0.95f, 1.2f);
+		audioSource.Play ();
+	}
+
+	void playShock () {
+		AudioClip shock = woodShocks [Random.Range (0, woodShocks.Length)];
+		audioSourceHit.clip = shock;
+		audioSourceHit.pitch = Random.Range (0.95f, 1.05f);
+		audioSourceHit.Play ();
+	}
+
+	void playHurt () {
+		audioSourceHit.clip = hurt;
+		audioSourceHit.pitch = Random.Range (0.95f, 1.05f);
+		audioSourceHit.Play ();
 	}
 }
---
+++ Assets/UIController.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UIController : MonoBehaviour {
+
+	public Color DefaultColor;
+	public Color HintColor;
+	public Color ToPressColor;
+	public Color PressedColor;
+	public Color ErrorColor;
+
+	public GameObject Missed;
+	public GameObject Parade;
+
+	public GameObject TooLate;
+	public GameObject TooEarly;
+	public GameObject GameOver;
+
+	public GameObject KeyboardText;
+	public GameObject AzertyButton;
+	public GameObject QwertyButton;
+	public GameObject PubText;
+	public GameObject Menu;
+
+	public Text ScoreText;
+	public Text HealthText;
+
+	public Text ArmsUp;
+	public Text ArmsDown;
+	public Text ArmsLeft;
+	public Text ArmsRight;
+	public Text LegsUp;
+	public Text LegsDown;
+	public Text LegsLeft;
+	public Text LegsRight;
+
+	public GameController gc;
+
+	Dictionary<string, Text> buttonNameToText;
+
+	void Awake () {
+		buttonNameToText = new Dictionary<string, Text> ();
+		buttonNameToText.Add ("Arms.Up", ArmsUp);
+		buttonNameToText.Add ("Arms.Down", ArmsDown);
+		buttonNameToText.Add ("Arms.Left", ArmsLeft);
+		buttonNameToText.Add ("Arms.Right", ArmsRight);
+		buttonNameToText.Add ("Legs.Up", LegsUp);
+		buttonNameToText.Add ("Legs.Down", LegsDown);
+		buttonNameToText.Add ("Legs.Left", LegsLeft);
+		buttonNameToText.Add ("Legs.Right", LegsRight);
+
+
+	}
+
+	public void HighlightHint(string buttonName) {
+		buttonNameToText[buttonName].color = HintColor;
+	}
+
+	public void HighlightToPressColor(string buttonName) {
+		buttonNameToText[buttonName].color = ToPressColor;
+	}
+
+	public void HighlightPressedColor(string buttonName) {
+		buttonNameToText[buttonName].color = PressedColor;
+	}
+
+	public void HighlightErrorColor(string buttonName) {
+		buttonNameToText[buttonName].color = ErrorColor;
+	}
+
+	public void ResetColors () {
+		foreach (var pair in buttonNameToText) {
+			pair.Value.color = DefaultColor;
+		}
+	}
+
+	public void ShowMissed () {
+		Missed.SetActive(true);
+	}
+
+	public void HideMissed () {
+		Missed.SetActive(false);
+	}
+
+	public void ShowParade () {
+		Parade.SetActive(true);
+	}
+
+	public void HideParade () {
+		Parade.SetActive(false);
+	}
+
+	public void ShowTooLate () {
+		TooLate.SetActive (true);
+	}
+
+	public void HideTooLate () {
+		TooLate.SetActive (false);
+	}
+
+	public void ShowTooEarly () {
+		TooEarly.SetActive (true);
+	}
+
+	public void HideTooEarly () {
+		TooEarly.SetActive (false);
+	}
+
+	public void ResetToNomalSituation () {
+		HideMissed ();
+		HideParade ();
+		HideTooLate ();
+		HideTooEarly ();
+		ResetColors ();
+		HideGameOver ();
+	}
+
+	public void UpdateScore (int score) {
+		ScoreText.text = "Score : " + score;
+	}
+
+	public void UpdateHealth (int health) {
+		HealthText.text = "Health : " + health;
+	}
+
+	public void ShowGameOver () {
+		GameOver.SetActive(true);
+		PubText.SetActive (true);
+	}
+
+	public void HideGameOver () {
+		GameOver.SetActive(false);
+		PubText.SetActive (false);
+	}
+
+	public void AzertyButtonPressed () {
+		HideInputDemand ();
+		DisplayMenu ();
+	}
+
+	public void QwertyButtonPressed () {
+		HideInputDemand ();
+		LegsUp.text = "W";
+		LegsDown.text = "S";
+		LegsLeft.text = "A";
+		LegsRight.text = "D";
+		DisplayMenu ();
+	}
+
+	public void DisplayMenu () {
+		Menu.SetActive (true);
+	}
+
+	public void StartGame () {
+		HideMenu ();
+		gc.StartGame ();
+	}
+
+	public void HideInputDemand () {
+		QwertyButton.SetActive (false);
+		AzertyButton.SetActive (false);
+		KeyboardText.SetActive (false);
+	}
+
+	public void HideMenu () {
+		PubText.SetActive(false);
+		Menu.SetActive(false);
+	}
+}
